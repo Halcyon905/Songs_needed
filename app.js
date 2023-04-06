c016dda50117a50b4dfe2a4e426ea88c012fe623
Napasakorn Boonkerd <89629829+Savetang19@users.noreply.github.com>
2023-04-06 18:34:33 +0700

    done update part

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const {MongoClient} = require('mongodb');
+const {MongoClient, Int32} = require('mongodb');
 
 var uri = 'mongodb://localhost:27017';
 const client = new MongoClient(uri);
@@ -6,6 +6,7 @@ const client = new MongoClient(uri);
 var express = require("express");
 var app = express();
 
+app.use(express.static('public'))
 app.set ( "view engine", "ejs" );
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: false}));
@@ -23,6 +24,8 @@ async function searchSong(doc){
     return dict;
 };
 
+// async function updateSong()
+
 app.get ("/search", async function(req, res) {
     res.render('myform');
 });
@@ -38,7 +41,6 @@ app.post ("/search/formsave", async function(req, res) {
     }
     if (req.body.artist != "") {
         var artist = await artistCollection.findOne({A_Name: req.body.artist});
-        console.log(artist);
         query["A_ID"] = artist.A_ID;
     }
     if (req.body.genre != "") {
@@ -47,9 +49,73 @@ app.post ("/search/formsave", async function(req, res) {
     }
 
     result = await searchSong(query);
-    res.send(result);
+    res.render("results", {
+        ResultSong: result
+    })
 });
 
-app.listen(8082, "127.0.0.1", () => {
+app.post ("/update", async function(req, res){
+    songs_needed = await client.db("Songs_Needed");
+    songCollection = songs_needed.collection("Song");
+    artistCollection = songs_needed.collection("Artist");
+    genreCollection = songs_needed.collection("Genre");
+
+    var song = await songCollection.findOne({S_ID: req.body.songID});
+
+    var artist = []
+
+    var genre = []
+
+    for await (const aid of song["A_ID"]) {
+        var ls = await artistCollection.findOne({A_ID: aid});
+        artist.push(ls.A_Name);
+    }
+
+    for await (const gid of song["G_ID"]) {
+        var ls = await genreCollection.findOne({G_ID: gid})
+        genre.push(ls.G_name)
+    }
+
+    song["A_Name"] =  artist
+    song["G_Name"] = genre
+
+    console.log(song)
+    res.render("update", {song})
+})
+
+app.post("/update/formsave", async function(req, res){
+    songs_needed = await client.db("Songs_Needed");
+    songCollection = songs_needed.collection("Song");
+    artistCollection = songs_needed.collection("Artist");
+    genreCollection = songs_needed.collection("Genre");
+
+    var artist = []
+
+    var genre = []
+
+    for await (const aname of req.body.A_name.split(',')) {
+        var ls = await artistCollection.findOne({A_Name: aname});
+        artist.push(ls.A_ID);
+    }
+
+    for await (const gname of req.body.G_name.split(',')) {
+        var ls = await genreCollection.findOne({G_name: gname});
+        genre.push(ls.G_ID);
+    }
+
+    var song = await songCollection.findOneAndUpdate({S_ID: req.body.S_ID}, 
+        {$set: {
+            S_name: req.body.S_name,
+            Year: parseInt(req.body.Year),
+            album: req.body.album,
+            A_ID: artist,
+            G_ID: genre
+        }});
+
+    res.redirect("/search")
+
+})
+
+app.listen(8082, () => {
     console.log("URL: http://127.0.0.1:8082/search")}
-    );
+);
