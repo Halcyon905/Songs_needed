79e7045e0a53321b9f43d9af6975de13245ce472
Punn Chunwimaleung <khunpunn2546@gmail.com>
2023-04-06 15:04:22 +0700

    done search function

--- app.js
+++ app.js
@@ -6,11 +6,15 @@ const client = new MongoClient(uri);
 var express = require("express");
 var app = express();
 
-async function searchSong(client, doc){
+app.set ( "view engine", "ejs" );
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: false}));
+
+async function searchSong(doc){
     songs_needed = await client.db("Songs_Needed");
     song = songs_needed.collection("Song");
 
-    test = await song.aggregate([{$match : {G_ID: doc}}]);
+    test = await song.aggregate([{$match: doc}]);
 
     var dict = {};
     for await (const song of test) {
@@ -20,9 +24,29 @@ async function searchSong(client, doc){
 };
 
 app.get ("/search", async function(req, res) {
-    await client.connect();
-    result = await searchSong(client, "01");
+    res.render('myform');
+});
 
+app.post ("/search/formsave", async function(req, res) {
+    songs_needed = await client.db("Songs_Needed");
+    artistCollection = songs_needed.collection("Artist");
+    genreCollection = songs_needed.collection("Genre");
+
+    var query = {};
+    if (req.body.title != "") {
+        query["S_name"] = req.body.title;
+    }
+    if (req.body.artist != "") {
+        var artist = await artistCollection.findOne({A_Name: req.body.artist});
+        console.log(artist);
+        query["A_ID"] = artist.A_ID;
+    }
+    if (req.body.genre != "") {
+        var genre = await genreCollection.findOne({G_name: req.body.genre});
+        query["G_ID"] = genre.G_ID;
+    }
+
+    result = await searchSong(query);
     res.send(result);
 });
 
